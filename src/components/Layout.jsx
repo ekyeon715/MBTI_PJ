7d49e9b25ca3e4a63072f52429efbb76396b025b
ekyeon715 <rkdwldus9636@naver.com>
2024-09-10 15:58:14 +0900

    feat: 다른 페이지로 이동하는 버튼 추가

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,22 +1,15 @@
-import { useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useUserStore from "../zustand/useUserStore";
 
 const Layout = ({ children }) => {
-  const { user } = useUserStore();
+  const { user, clearUser } = useUserStore();
   const navigate = useNavigate();
 
   // 이곳에서 로그인 하지 않은 사용자를 login 페이지로 보내줄 거에요.
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      // 로그인하지 않은 사용자라면 로그인 페이지로 리디렉션
-      navigate("/login");
-    }
-  }, []);
 
   const handleLogout = () => {
     localStorage.removeItem("token");
+    clearUser();
     navigate("/login");
   };
 
@@ -29,6 +22,9 @@ const Layout = ({ children }) => {
             {user ? (
               <>
                 {/*다른 페이지로 가는 버튼도 필요할 겁니다.*/}
+                <Link to="/profile">프로필</Link>
+                <Link to="/test">테스트</Link>
+                <Link to="/results">결과페이지</Link>
                 <button onClick={handleLogout}>로그아웃</button>
               </>
             ) : (
