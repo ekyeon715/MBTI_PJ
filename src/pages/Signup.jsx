85fbd408de8aba506f8138f2731669ee3e3b3fe1
ekyeon715 <rkdwldus9636@naver.com>
2024-09-10 09:45:01 +0900

    feat: 회원가입 페이지 및 기능 추가

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,33 @@
+import AuthForm from "../components/AuthForm";
+import { Link, useNavigate } from "react-router-dom";
+import { register } from "../api/auth";
+
+const Signup = () => {
+  const navigate = useNavigate();
+
+  const handleSignup = async (formData) => {
+    try {
+      await register(formData);
+      alert("회원가입에 성공했습니다. 로그인페이지로 이동합니다.");
+      navigate("/login");
+    } catch (error) {
+      alert("회원가입에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>회원가입</h1>
+        <AuthForm mode="signup" onSubmit={handleSignup} />
+        <div>
+          <p>
+            이미 계정이 있으신가요? <Link to="/login">로그인</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signup;
