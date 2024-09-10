fd9435f28532be8ebfc0fbeb1f0c8860c777f579
ekyeon715 <rkdwldus9636@naver.com>
2024-09-10 09:44:47 +0900

    feat: 로그인 페이지 및 기능 추가

---
+++ src/pages/Login.jsx
@@ -0,0 +1,37 @@
+import AuthForm from "../components/AuthForm";
+import { Link, useNavigate } from "react-router-dom";
+import useUserStore from "../zustand/useUserStore";
+import { login } from "../api/auth";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const { setUser } = useUserStore();
+
+  const handleLogin = async (formData) => {
+    try {
+      const response = await login(formData);
+      const user = response.data;
+      setUser(user);
+      navigate("/");
+      alert("로그인에 성공했습니다.");
+    } catch (error) {
+      alert("로그인에 실패했습니다. 다시 시도해주세요.");
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <h1>로그인</h1>
+        <AuthForm mode="login" onSubmit={handleLogin} />
+        <div>
+          <p>
+            계정이 없으신가요? <Link to="/signup">회원가입</Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
